67ae322c6e8556a69a18a0c4e435362bf7f29e05
Kenny Younger <kyounger@gmail.com>
2019-02-22 13:09:12 -0600

    Add index controller

---
+++ src/main/java/com/example/springboot2/MainController.java
@@ -0,0 +1,13 @@
+package com.example.springboot2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, world.";
+    }
+}
